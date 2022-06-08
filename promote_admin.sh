8fbf60798aed7c5e0af459b24f77f2904dbe2192
jasonarias <46569817+jasonarias@users.noreply.github.com>
2022-06-08 09:37:53 -0500

    remove python code

--- promote_admin.sh
+++ promote_admin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#This no longer works due to python2 deprecation
+#user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#Instead:
+user=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ {print $3}') 
 # nonadmingroups="staff"  # for a non-admin user
 # admingroups="admin _appserveradm _appserverusr" # for an admin user
 sudo dscl . -append /Groups/admin GroupMembership "$user"
