4e70c7e34ac70bb474b7ed567f8accd5d641d60a
jasonarias <46569817+jasonarias@users.noreply.github.com>
2022-06-08 08:54:48 -0500

    remove python code
    Python2 was removed in MacOS 12.3; use scutil instead to find currently logged-in user

--- set_user_icons.sh
+++ set_user_icons.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # vars
-user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#This no longer works due to python2 deprecation
+#user=$(python3 -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#Instead:
+user=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ {print $3}') 
 directory=$HOME/.artsy
 image="https://github.com/jasonarias/2021onboarding/blob/main/user.tif?raw=true"
 
