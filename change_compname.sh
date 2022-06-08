fd2d46dfe3f2848247d9244a886bb0c35fa3666d
jasonarias <46569817+jasonarias@users.noreply.github.com>
2022-06-08 09:10:08 -0500

    Remove python code

--- change_compname.sh
+++ change_compname.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 serial=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')
-user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#This no longer works due to python2 deprecation
+#user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#Instead:
+user=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ {print $3}')
 username=$(echo "${user}" | sed -e 's/\./-/g')
 compname=${username}-${serial} # need some cut/awk command to keep characters less than say 50 just in case
 
